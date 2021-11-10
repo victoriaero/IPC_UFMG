e1176a215f1dac4ced4dcbc6f5358923b3e3962a
Victoria Estanislau <git init>
2021-11-09 21:23:58 -0300

    Correcao problema 1/ pratica 2

--- Pratica2/problema1/problema1.py
+++ Pratica2/problema1/problema1.py
@@ -6,32 +6,30 @@ n3 = int(input("Digite o terceiro inteiro: "))
 n4 = int(input("Digite o quarto inteiro: "))
 n5 = int(input("Digite o quinto inteiro: "))
 
-count_maior = int()
-count_menor = int()
 count_div3 = int()
 
 if n1 >= n2 and n1 >= n3 and n1 >= n4 and n1 >= n5:
-    count_maior += 1
-if n2 >= n1 and n2 >= n3 and n2 >= n4 and n2 >= n5:
-    count_maior += 1
-if n3 >= n1 and n3 >= n2 and n3 >= n4 and n3 >= n5:
-    count_maior += 1
-if n4 >= n1 and n4 >= n2 and n4 >= n3 and n4 >= n5:
-    count_maior += 1
-if n5 >= n1 and n5 >= n2 and n5 >= n3 and n5 >= n4:
-    count_maior += 1
+    print("Maior: " + str(n1))
+elif n2 >= n1 and n2 >= n3 and n2 >= n4 and n2 >= n5:
+    print("Maior: " + str(n2))
+elif n3 >= n1 and n3 >= n2 and n3 >= n4 and n3 >= n5:
+    print("Maior: " + str(n3))
+elif n4 >= n1 and n4 >= n2 and n4 >= n3 and n4 >= n5:
+    print("Maior: " + str(n4))
+elif n5 >= n1 and n5 >= n2 and n5 >= n3 and n5 >= n4:
+    print("Maior: " + str(n5))
 
 # menor número
 if n1 <= n2 and n1 <= n3 and n1 <= n4 and n1 <= n5:
-    count_menor += 1
-if n2 <= n1 and n2 <= n3 and n2 <= n4 and n2 <= n5:
-    count_menor += 1
-if n3 <= n1 and n3 <= n2 and n3 <= n4 and n3 <= n5:
-    count_menor += 1
-if n4 <= n1 and n4 <= n2 and n4 <= n3 and n4 <= n5:
-    count_menor += 1
-if n5 <= n1 and n5 <= n2 and n5 <= n3 and n5 <= n4:
-    count_menor += 1
+    print("Menor: " + str(n1))
+elif n2 <= n1 and n2 <= n3 and n2 <= n4 and n2 <= n5:
+    print("Menor: " + str(n2))
+elif n3 <= n1 and n3 <= n2 and n3 <= n4 and n3 <= n5:
+    print("Menor: " + str(n3))
+elif n4 <= n1 and n4 <= n2 and n4 <= n3 and n4 <= n5:
+    print("Menor: " + str(n4))
+elif n5 <= n1 and n5 <= n2 and n5 <= n3 and n5 <= n4:
+    print("Menor: " + str(n5))
 
 # divisíveis por três
 if n1%3 == 0:
@@ -45,6 +43,4 @@ if n4%3 == 0:
 if n5%3 == 0:
     count_div3 += 1
 
-print("Maior: " + str(count_maior))
-print("Menor: " + str(count_menor))
 print("Quantidade de divisíveis por 3: " + str(count_div3))
